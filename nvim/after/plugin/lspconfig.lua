51508d5b0e56f1b5c27afa4ac4fddab122179975
Miguel Ciulog <angel.ciulog7@gmail.com>
2022-09-18 20:27:43 -0700

    Fixed stuff

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -4,21 +4,17 @@ if (not status) then return end
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap = true, silent = true }
-
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  -- vim.keymap.set('n', '<leader>gd', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', '<leader>gd', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  -- vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
 local protocol = require('vim.lsp.protocol')
@@ -76,7 +72,19 @@ vim.diagnostic.config({
 })
 
 -- Set up languages
-require('lspconfig').pyright.setup{}
-require('lspconfig').gopls.setup{}
-require('lspconfig').jsonls.setup{}
-require('lspconfig').tsserver.setup{}
+require('lspconfig').pyright.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig').gopls.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig').jsonls.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig').tsserver.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
