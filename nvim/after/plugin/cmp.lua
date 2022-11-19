97d947ee2853db04439c6e2fe3edc9aa8e54577a
Miguel Ciulog <angel.ciulog7@gmail.com>
2022-11-19 00:36:36 -0700

    Update

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -1,6 +1,6 @@
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- luasnip setup
@@ -55,10 +55,11 @@ cmp.setup {
         end, { 'i', 's' }),
     }),
     sources = {
-        { name = "nvim_lsp" },
         { name = "luasnip" },
-        { name = "path", keyword_length = 5 },
-        { name = "buffer" , keyword_length = 3 },
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "nvim_lua" },
+        { name = "path" },
     },
     formatting = {
         format = lspkind.cmp_format({
