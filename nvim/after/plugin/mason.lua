9c49fe8c0dc92d9a417bd36b34e94a12e38849fb
MiguelCiulog <36050843+MiguelCiulog@users.noreply.github.com>
2023-03-14 17:51:59 -0700

    update

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -9,7 +9,7 @@ require("mason").setup({
 })
 require("mason-lspconfig").setup({
     ensure_installed={
-        "sumneko_lua",
+        "lua_ls",
         "pyright",
         "gopls",
         "jsonls",
