39b10b7879260d2bf8dc685ed1d1d91dcf18844d
root <root@akuwin>
2023-10-16 23:26:40 -0700

    fzf

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,20 +1,25 @@
 local map = require("aku.binds").map
 
-require('telescope').setup{
-    defaults = {
-        file_ignore_patterns = {
-            "node_modules",
-            ".git",
-            "venv",
-            "__pycache__",
-        }
-    }
-}
-require('telescope').load_extension('fzf')
+require("telescope").setup({
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+	},
+	defaults = {
+		file_ignore_patterns = {
+			"node_modules",
+			".git",
+			"venv",
+			"__pycache__",
+		},
+	},
+})
+require("telescope").load_extension("fzf")
 
 -- Find files using Telescope command-line sugar.
-map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-map('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
-map('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>', {silent=true})
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+map("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+map("n", "gr", '<cmd>lua require("telescope.builtin").lsp_references()<cr>', { silent = true })
