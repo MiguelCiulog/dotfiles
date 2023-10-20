7130d15e3c4f803b7c758df1f1ae5b433f0612c8
MiguelCiulog <36050843+MiguelCiulog@users.noreply.github.com>
2023-10-19 18:54:31 -0700

    removed borders

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,45 +1,45 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(_, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = bufnr})
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = bufnr})
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {buffer = bufnr})
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = bufnr})
-    vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr })
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr })
+	vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { buffer = bufnr })
 
-    vim.keymap.set("n", "[e", vim.diagnostic.goto_prev, {buffer = bufnr})
-    vim.keymap.set("n", "]e", vim.diagnostic.goto_next, {buffer = bufnr})
+	vim.keymap.set("n", "[e", vim.diagnostic.goto_prev, { buffer = bufnr })
+	vim.keymap.set("n", "]e", vim.diagnostic.goto_next, { buffer = bufnr })
 
-    vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, {buffer = bufnr})
-    vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action, {buffer = bufnr})
+	vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { buffer = bufnr })
+	vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action, { buffer = bufnr })
 
-    vim.keymap.set('n', '<space>fr', function()
-        vim.lsp.buf.format { async = true }
-    end, {buffer = bufnr})
+	vim.keymap.set("n", "<space>fr", function()
+		vim.lsp.buf.format({ async = true })
+	end, { buffer = bufnr })
 end)
 
-require('mason').setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        "lua_ls",
-        "pyright",
-        "gopls",
-        "jsonls",
-        "tsserver",
-    },
-    handlers = {
-        lsp_zero.default_setup,
-    },
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"pyright",
+		"gopls",
+		"jsonls",
+		"tsserver",
+	},
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 
 -- CMP
@@ -51,99 +51,107 @@ require('mason-lspconfig').setup({
 
 -- luasnip setup
 local luasnip = require("luasnip")
-require('luasnip.loaders.from_vscode').lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 
 -- lsp-kind setup (Pictograms)
-local lspkind = require 'lspkind'
+local lspkind = require("lspkind")
 
 -- nvim-cmp setup
-local cmp = require 'cmp'
-local cmp_action = require('lsp-zero').cmp_action()
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
-cmp.setup {
-    preselect = cmp.PreselectMode.None,
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered()
-    },
-    mapping = cmp.mapping.preset.insert({
-        -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+cmp.setup({
+	preselect = cmp.PreselectMode.None,
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	-- window = {
+	--     completion = cmp.config.window.bordered(),
+	--     documentation = cmp.config.window.bordered()
+	-- },
+	mapping = cmp.mapping.preset.insert({
+		-- `Enter` key to confirm completion
+		["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
 
-        -- Ctrl+Space to trigger completion menu
-        ['<C-Space>'] = cmp.mapping.complete(),
+		-- Ctrl+Space to trigger completion menu
+		["<C-Space>"] = cmp.mapping.complete(),
 
-        -- Navigate between snippet placeholder
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+		-- Navigate between snippet placeholder
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 
-        -- Scroll up and down in the completion documentation
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    }),
-    sources = {
-        { name = "luasnip" },
-        {
-            name = "nvim_lsp",
-            entry_filter = function(entry, _)
-                return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
-            end
-        },
-        {
-            name = "buffer",
-            keyword_length = 5,
-        },
-        { name = "nvim_lua" },
-        { name = "path" },
-    },
-    formatting = {
-        fields = { 'abbr', 'kind', 'menu' },
-        format = lspkind.cmp_format({
-            mode = 'symbol',       -- show only symbol annotations
-            maxwidth = 50,         -- prevent the popup from showing more than provided characters
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-        })
-    }
-}
+		-- Scroll up and down in the completion documentation
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+	}),
+	sources = {
+		{ name = "luasnip" },
+		{
+			name = "nvim_lsp",
+			entry_filter = function(entry, _)
+				return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
+			end,
+		},
+		{
+			name = "buffer",
+			keyword_length = 5,
+		},
+		{ name = "nvim_lua" },
+		{ name = "path" },
+	},
+	formatting = {
+		fields = { "abbr", "kind", "menu" },
+		format = lspkind.cmp_format({
+			mode = "symbol", -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+		}),
+	},
+})
 
 -- Colors on letters
 -- gray
-vim.cmd "highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080"
+vim.cmd("highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080")
 -- blue"
-vim.cmd "highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6"
-vim.cmd "highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6"
+vim.cmd("highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6")
+vim.cmd("highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6")
 -- light" blue"
-vim.cmd "highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE"
-vim.cmd "highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE"
-vim.cmd "highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE"
+vim.cmd("highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE")
+vim.cmd("highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE")
+vim.cmd("highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE")
 -- pink"
-vim.cmd "highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0"
-vim.cmd "highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0"
+vim.cmd("highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0")
+vim.cmd("highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0")
 -- front""
-vim.cmd "highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4"
-vim.cmd "highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4"
-vim.cmd "highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4"
+vim.cmd("highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4")
+vim.cmd("highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4")
+vim.cmd("highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4")
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
-    virtual_text = {
-        prefix = "●",
-    },
-    update_in_insert = true,
-    float = {
-        source = "always", -- Or "if_many"
-    },
+	virtual_text = {
+		prefix = "●",
+	},
+	update_in_insert = true,
+	float = {
+		source = "always", -- Or "if_many"
+	},
+})
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = "shadow",
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = "none",
 })
 
 -- EXTRA LSP FUNCTIONAL SETTINGS
