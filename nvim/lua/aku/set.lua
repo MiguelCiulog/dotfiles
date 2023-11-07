c8bbd6a9c49984b0c0b97486cde35655026e01ab
MiguelCiulog <angel.ciulog7@gmail.com>
2023-11-06 21:21:34 -0700

    added basic neovide support

--- nvim/lua/aku/set.lua
+++ nvim/lua/aku/set.lua
@@ -46,3 +46,20 @@ vim.opt.scrolloff = 8
 
 vim.opt.list = true
 vim.opt.background = "dark"
+
+if vim.g.neovide then
+    vim.o.guifont = "Mononoki_Nerd_Font_Mono:h13"
+
+    vim.g.neovide_transparency = 0.97
+    vim.g.neovide_scroll_animation_length = 0.1
+
+    vim.g.neovide_cursor_animation_length = 0.05
+    vim.g.neovide_cursor_trail_size = 0.5
+
+      vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+      vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+      vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+      vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+      vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
