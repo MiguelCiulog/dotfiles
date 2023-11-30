eee5b94c54dfae7aa04e5e8c13d485939ee57cfa
MiguelCiulog <angel.ciulog7@gmail.com>
2023-11-29 17:00:42 -0700

    la bebestia

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -19,7 +19,6 @@ require("telescope").setup({
 
 -- Find files using Telescope command-line sugar.
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-map("n", "<c-p>", "<cmd>Telescope find_files<cr>")
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 map("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
