e0d3653bb6624e45ec99bd5fdd78058f4ea419e7
MiguelCiulog <angel.ciulog7@gmail.com>
2024-05-04 18:49:47 -0700

    removed lspsaga

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,8 +2,6 @@ return {
   "neovim/nvim-lspconfig",
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- change a keymap
-    keys[#keys + 1] = { "K", "<cmd>Lspsaga hover_doc<CR>" }
 
     -- disable a keymap
     keys[#keys + 1] = { "<leader>cl", false }
