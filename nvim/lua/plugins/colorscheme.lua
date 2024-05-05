1214b3a06f8908c0f6765e90a56805dd31a34d71
MiguelCiulog <angel.ciulog7@gmail.com>
2024-05-04 18:49:58 -0700

    newasodkjfalsjk

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,16 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "felipeagc/fleet-theme-nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "kanagawa-dragon",
+      colorscheme = "fleet",
     },
   },
 }
