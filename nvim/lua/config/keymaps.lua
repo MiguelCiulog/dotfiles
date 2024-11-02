010cf3aa0acdeece38ee8595e40cb61161f52fa0
Miguel Ciulog <angel.ciulog7@gmail.com>
2024-11-02 15:41:59 -0700

    copy to github

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -54,6 +54,38 @@ map("i", "<C-l>", "<End>")
 -- formatting
 map("n", "==", "<Cmd>LazyFormat<CR>", { desc = "Format" })
 
+vim.api.nvim_create_user_command("CopyFullPath", function()
+    local path = vim.fn.expand("%:p")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
+
+vim.api.nvim_create_user_command("CopyRelPath", function()
+    local path = vim.fn.expand("%")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
+
+vim.api.nvim_create_user_command("CopyGithubCurrFilePath", function()
+    -- Get the GitHub repository URL, removing the ".git" suffix
+    local repo_url = vim.fn.system("git config --get remote.origin.url | sed -e 's/\\.git$//'")
+    repo_url = repo_url:gsub("\n", "")
+
+    -- Get the relative path of the current file in the repository (we use git ls-files in case we are not on the git root directory)
+    local file_path = vim.fn.system("git ls-files --full-name " .. vim.fn.expand("%"))
+    file_path = file_path:gsub("\n", "")
+
+    -- Get the current branch name
+    local branch_name = vim.fn.system("git rev-parse --abbrev-ref HEAD")
+    branch_name = branch_name:gsub("\n", "")
+
+    -- Build the full GitHub URL to the file
+    local github_url = string.format("%s/blob/%s/%s", repo_url, branch_name, file_path)
+
+    vim.fn.setreg("+", github_url)
+    vim.notify('Copied "' .. github_url .. '" to the clipboard!')
+end, {})
+
 -----------------------------------------------------
 -------------- Removing cringe keymaps --------------
 -----------------------------------------------------
