67e0464301502f14d7948db90ffd7883a9f4a6bd
Miguel Ciulog <angel.ciulog7@gmail.com>
2025-10-27 11:50:02 -0700

    asd;flkj

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,10 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.relativenumber = true
+-- vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+-- vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+-- vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+-- vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 -- Disable LazyVim auto format
 vim.g.autoformat = false
@@ -14,20 +15,9 @@ vim.opt.colorcolumn = "100"
 
 vim.opt.clipboard = ""
 
-if vim.g.neovide then
-  vim.o.guifont = "Mononoki_Nerd_Font_Mono:h13"
-
-  vim.g.neovide_transparency = 0.97
-  vim.g.neovide_scroll_animation_length = 0.1
-
-  vim.g.neovide_cursor_animation_length = 0.05
-  vim.g.neovide_cursor_trail_size = 0.5
-
-  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
-end
-
 vim.g.snacks_animate = false
+
+-- LSP Server to use for Ruby.
+-- Set to "solargraph" | "ruby_lsp".
+vim.g.lazyvim_ruby_lsp = "solargraph"
+vim.g.lazyvim_ruby_formatter = "rubocop"
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,10 +1,3 @@
 return {
-  -- disable trouble
-  -- { "noice.nvim", enabled = false },
-  -- { "todo-comments.nvim", enabled = false },
-  -- { "trouble.nvim", enabled = false },
-  -- { "dressing.nvim", enabled = false},
-  -- { "nvim-notify", enabled = false},
-  -- { "markdown-preview.nvim", enabled = false}
   {"mini.pairs", enabled = false}
 }
